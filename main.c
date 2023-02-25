d4d40ad6d425992398995ac942704acaf914c78c
ysl2 <www.songli.yu@gmail.com>
2023-02-25 10:16:46 +0800

    1

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -60,10 +61,26 @@ int func02()
     return 0;
 }
 
+typedef unsigned char *pointer;
+void show_bytes(pointer start, size_t len)
+{
+    size_t i;
+    for (i = 0; i < len; i++)
+    {
+        printf("%p\t0x%.2x\n", start + i, start[i]);
+    }
+    printf("\n");
+}
+
 int main(int argc, char *argv[])
 {
     // func();
     // func01();
-    func02();
+    // func02();
+
+    int a = 15213;
+    printf("int a = 15213;\n");
+    show_bytes((pointer)&a, sizeof(int));
+
     return 0;
 }
